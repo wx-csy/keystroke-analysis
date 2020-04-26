4b1895629809c9609b165584f3040a7793ff27aa
Shaoyuan Chen <sy_chen@smail.nju.edu.cn>
2020-04-26 16:23:12 +0800

    add op stats

--- postproc.py
+++ postproc.py
@@ -8,6 +8,10 @@ class Counter(dict) :
 charset = set()
 digram = Counter()
 trigram = Counter()
+insert = Counter()
+delete = Counter()
+replace = Counter()
+swap = Counter()
 
 tot_sentence = 0
 tot_freq = 0
@@ -25,6 +29,19 @@ for sentence, freq in csv.reader(open('sentences.txt'), delimiter='\t') :
 
 charset = ''.join(sorted(charset))
 
+for op, freq in csv.reader(open('ops.txt'), delimiter='\t') :
+    freq = int(freq)
+    op, old = op.split(' ', 1)
+    old, new = old.split(' -> ', 1)
+    if op == 'Insert' :
+        insert[new] += 1
+    elif op == 'Delete' :
+        delete[old] += 1
+    elif op == 'Swap' :
+        swap[new] += 1
+    elif op == 'Replace' :
+        replace[new[0] + new[1] + old[1] + new[2]] += 1
+
 laplace = tot_freq // (2 * tot_sentence) + 1
 
 stat = {
@@ -32,6 +49,12 @@ stat = {
     'laplace': laplace,
     'digram': dict(digram), 
     'trigram': dict(trigram),
+    'edit' : {
+        'replace': dict(replace),
+        'insert': dict(insert),
+        'delete': dict(delete),
+        'swap': dict(swap)
+    }
 }
 
 yaml.safe_dump(stat, open('result.yml', 'w'))
